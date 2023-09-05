28ff5a70b52b2a3794c4d7229c67d4c70b428ef2
GuramDo <120183388+GuramDo@users.noreply.github.com>
2023-09-05 17:32:42 +0400

    init

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Guga Dolidze on 9/5/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
